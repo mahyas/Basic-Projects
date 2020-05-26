2d97e5c6b0ea270ec56a85a108f80d1370fa322d
mahyas <mahyas@github.com>
2020-05-25 23:51:46 -0700

    renamed js basic login generalized

--- Simple Login Page/script.js
+++ Simple Login Page/script.js
@@ -2,11 +2,19 @@ var database = [
     {
         username: "felfel",
         password: "peppermint"
-    }   
+    },
+    {
+        username: "honey",
+        password: "jacobs"
+    },
+    {
+        username: "jasper",
+        password: "carrot"
+    }    
 ];
 
 var timeline = [
-    {
+    { 
         username: "macaron",
         news: "Today I played a lot"
     },   
@@ -19,9 +27,18 @@ var timeline = [
 var userNamePrompt = prompt("what's your username?");
 var passwordPrompt = prompt("what's your password?");
 
+function isUserValid(user, pass) {
+    for(i=0;i<database.length;i++){
+        if(user === database[i].username &&
+            pass === database[i].password) {
+               return true;
+           } 
+           return false;
+    }
+}
+
 function logIn(user, pass) {
-    if(user === database[0].username &&
-        pass === database[0].password) {
+    if(isUserValid(user,pass)){
             console.log(timeline);
         } else {
             alert("sorry, username and/or password is incorrect");
